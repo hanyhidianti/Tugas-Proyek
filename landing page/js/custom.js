9701f441d8a8b8746453d37bc423e183be8c7ea7
hanyhidianti <hanyhidianti123@gmail.com>
2022-11-23 14:00:59 +0700

    Tugas-Proyek

---
+++ landing page/js/custom.js
@@ -0,0 +1,22 @@
+// to get current year
+function getYear() {
+    var currentDate = new Date();
+    var currentYear = currentDate.getFullYear();
+    document.querySelector("#displayYear").innerHTML = currentYear;
+}
+
+getYear();
+
+// nice select
+$(document).ready(function () {
+    $('select').niceSelect();
+});
+
+// date picker
+$(function () {
+    $("#inputDate").datepicker({
+        autoclose: true,
+        todayHighlight: true
+    }).datepicker('update', new Date());
+});
+
